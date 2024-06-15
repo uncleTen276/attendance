113ea369d3db36f78b25493361b8b622e87f66ac
sekke276 <tringuyen2762001@gmail.com>
2024-06-15 15:22:55 +0700

    rebase

--- contract/scripts/deploy.ts
+++ contract/scripts/deploy.ts
@@ -1,12 +1,27 @@
 import { ethers } from "hardhat";
-
-const ether = require("hardhat").ethers;
-
+import fs from "fs";
+import path from "path";
 async function main() {
   const Attendance = await ethers.getContractFactory("Attendance");
   const contract = await Attendance.deploy();
   const address = await contract.getAddress();
-  console.log(`Contract Address, ${address}`);
+  // write to env file
+  const envFilePath = path.resolve(__dirname, "..", "..", "server", ".env");
+  let envConfig = fs.existsSync(envFilePath)
+    ? fs.readFileSync(envFilePath, "utf8")
+    : "";
+  const newContent = `CONTRACT_ADDRESS=${address}\n`;
+  if (envConfig.includes(`CONTRACT_ADDRESS=`)) {
+    envConfig = envConfig.replace(
+      `CONTRACT_ADDRESS=`,
+      `CONTRACT_ADDRESS=${newContent}`
+    );
+  } else {
+    envConfig += newContent;
+  }
+
+  fs.writeFileSync(envFilePath, envConfig, "utf8");
+  console.log(`Contract deployed to ${address}`);
 }
 
 main().catch((error) => {
